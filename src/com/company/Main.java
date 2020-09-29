9fbafcf82af2bf2b5459e9885ebd550a2e00b087
knsnedden <kayleah.snedden@trojans.dsu.edu>
2020-09-29 15:02:08 -0700

    bruteForce implemented

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,20 @@
 package com.company;
 
 import java.util.Random;
-
+import java.util.ArrayList;
+import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
         int[] arr;
-        int size = 10;
+        int size = 20;
+        ArrayList<int[]> fin = new ArrayList<int[]>();
 
         arr = generateList(size);
+        Arrays.sort(arr);
+
+        fin = bruteForce(arr);
+
     }
 
     public static int[] generateList(int N){
@@ -22,5 +28,27 @@ public class Main {
         }
 
         return arr;
+    }
+
+    public static ArrayList<int[]> bruteForce(int[] arr){
+        ArrayList<int[]> result = new ArrayList<int[]>();
+
+        for (int i = 0; i < arr.length; ++i){
+            for (int j = i + 1; j < arr.length; ++j){
+                for (int k = j + 1; k < arr.length; ++k){
+                    if (arr[i] + arr[j] + arr[k] == 0){
+                        if (arr[i] != arr[j] && arr[i] != arr[k] && arr[j] != arr[k]){
+                            int[] hold = new int[3];
+                            hold[0] = arr[i];
+                            hold[1] = arr[j];
+                            hold[2] = arr[k];
+                            result.add(hold);
+                        }
+                    }
+
+                }
+            }
+        }
+        return result;
     }
 }
