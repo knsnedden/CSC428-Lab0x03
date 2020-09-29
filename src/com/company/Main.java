76182f45afc17866b8081ee6adcf1050ca7b9df6
knsnedden <kayleah.snedden@trojans.dsu.edu>
2020-09-29 14:07:28 -0700

    generateList implemented

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,26 @@
 package com.company;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int[] arr;
+        int size = 10;
+
+        arr = generateList(size);
+    }
+
+    public static int[] generateList(int N){
+        Random randNum = new Random();
+        int[] arr = new int[N];
+        int x;
+
+        for (int i = 0; i < N; ++i){
+            x = randNum.nextInt((2*N)-(-2*N)) - (2*N);
+            arr[i] = x;
+        }
+
+        return arr;
     }
 }
